88f4a020a6bb8082d009937425c803a0bcab3f39
Matheus Antunes <matheusbyte0101@gmail.com>
2024-03-29 22:54:26 -0300

    Exercicio proposto em aula

--- Exercicios propostos em aula/script.js
+++ Exercicios propostos em aula/script.js
@@ -1,14 +1,17 @@
 data = new Date()
 hora = data.getHours()
-var body = document.body 
+var body = document.body
 
 document.getElementById(`phora`).innerText = `Agora são ${hora} horas.`
 
-if(hora < 12){
-    body.style.background = `green`
-    
-}else if (hora < 18 & hora > 12){
-    body.style.background = `red` 
+if(hora <= 11){
+    body.style.background = `#fffbe8`
+    body.style.color = `black`
+    document.getElementById(`fotododia`).src = `images/manha.png`
+}else if (hora < 18 & hora >= 12){
+    body.style.background = `##fff868` 
+    document.getElementById(`fotododia`).src = `images/tarde.png`
 }else{
-    body.style.background = `black` 
+    body.style.background = `#1c203b` 
+    document.getElementById(`fotododia`).src = `images/noite.png`
 }
